76f7728f40e40e678fd7f3c18fb9e9d61275a524
Lê Trung Vinh <letrungvinh99@gmail.com>
2020-10-19 21:38:51 +0700

    2nd

--- transfer/urls.py
+++ transfer/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('/form/',views.form,name='form'),
     path('', views.index, name='index'),
 ]
--- transfer/views.py
+++ transfer/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'transfer/index.html')
+
+def form(request):
+    fid = request.POST['fid']
+    tid = request.POST['tid']
+    amount = request.POST['amount']
+    method = request.POST['method']
+    print("{} {} {} {}".format(fid,tid,amount,method))
+    return render(request, 'transfer/transfer.html')
